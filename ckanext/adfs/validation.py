f447606b1c2af90e73005b6f334329eddc82604f
Nicholas H.Tollervey <ntoll@ntoll.org>
2015-02-18 16:02:12 +0000

    Fixed and tidied up validation functions.

--- ckanext/adfs/validation.py
+++ ckanext/adfs/validation.py
@@ -4,8 +4,7 @@ Validation related functions.
 import logging
 import base64
 import lxml.etree as ET
-from StringIO import StringIO
-#from M2Crypto import EVP, RSA, X509, m2
+from M2Crypto import EVP, X509
 
 
 log = logging.getLogger(__name__)
@@ -13,7 +12,7 @@ log = logging.getLogger(__name__)
 
 def get_tag(doc, tagname):
     """
-    Assume that you don't care about namespaces
+    Assume that we don't care about namespaces or painful XML xpath search.
     """
     for t in doc.iter('*'):
         if t.tag.endswith(tagname):
@@ -37,9 +36,7 @@ def verify_signature(signed_info, cert, signature):
 
 def get_signature(doc):
     """
-    Ahahahahahahahaahahaha..!
-
-    Someone, somewhere is killing an XML kitten.
+    Get's the tag that includes the security token (signature).
     """
     return get_tag(doc, 'RequestedSecurityToken')
 
@@ -52,8 +49,6 @@ def get_signed_info(signature):
     why we need to use LXML).
     """
     signed_info = get_tag(signature, 'SignedInfo')
-    """signed_info = signature.find(
-            '{http://www.w3.org/2000/09/xmldsig#}SignedInfo')"""
     signed_info_str = ET.tostring(signed_info, method='c14n', exclusive=True)
     return signed_info_str
 
@@ -62,9 +57,6 @@ def get_cert(signature):
     """
     Gets the certificate from the eggsmell.
     """
-    """
-    keyinfo = get_tag(signature, 'KeyInfo')
-    keydata = get_tag(keyinfo, 'X509Data')"""
     certelem = get_tag(signature, 'X509Certificate')
     return certelem.text
 
@@ -76,25 +68,29 @@ def get_signature_value(signature):
     return get_tag(signature, 'SignatureValue').text
 
 
-def validate_saml(saml, x509):
+def validate_saml(saml, x509_certificates):
     """
     Given a string representation of a SAML response will return a boolean
     indication of if it's cryptographically valid (i.e. the signature
-    validates). The x509 argument is a string representation of the expected
-    certificate incoming in the SAML.
+    validates). The x509_certificates argument is a set containing string
+    representations of the expected valid certificates that could be referenced
+    in the incoming SAML.
     """
-    log.error('SAML')
-    log.error(saml)
     try:
         xml = ET.fromstring(saml)
         signature = get_signature(xml)
         signed_info = get_signed_info(signature)
         cert = get_cert(signature)
-        """if x509 != cert:
-            # Ensure the SAML certificate is the same as the expected cert.
-            return False"""
+        if cert not in x509_certificates:
+            # Ensure the SAML certificate is one of the valid ones provided
+            # by the metadata.
+            return False
         signature_value = get_signature_value(signature)
         is_valid = verify_signature(signed_info, cert, signature_value)
+        if is_valid != 1:
+            # To save some time in the future when we need to see broken SAML.
+            log.error('INVALID SAML')
+            log.error(saml)
         return is_valid==1
     except Exception as ex:
         # Log this for later consumption.
