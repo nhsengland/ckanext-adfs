96c469b6fe88df4d0f614f7e85670d09a08d4c76
Jay Guo <jguo144@gmail.com>
2023-03-03 16:21:37 -0500

    Block invalid usernames (#12)
    * Update schema and validators
    
    * Update validators
    
    Fix adfs_old_username_validator in cases where a sysadmin is the user making edits

--- ckanext/adfs/schema.py
+++ ckanext/adfs/schema.py
@@ -1,18 +1,22 @@
+from ckantoolkit import unicode_safe
+
 from ckan.lib.navl.validators import ignore, ignore_missing, not_empty
 from ckan.logic.validators import (
     ignore_not_sysadmin,
-    name_validator,
     user_about_validator,
     user_both_passwords_entered,
     user_name_validator,
     user_password_not_empty,
-    user_password_validator,
-    user_passwords_match
+    user_passwords_match,
+    email_validator
 )
 from ckanext.adfs.validators import (
+    adfs_user_password_validator,
+    adfs_old_username_validator,
+    adfs_user_name_sanitize,
     adfs_name_validator,
-    adfs_user_about_validator,
-    adfs_user_name_sanitize
+    adfs_password_validator,
+    json_object
 )
 
 
@@ -21,13 +25,15 @@ def default_user_schema():
     Custom schema to validate users
     """
     schema = {
-        'id': [ignore_missing, unicode],
-        'name': [not_empty, name_validator, user_name_validator, unicode],
-        'fullname': [ignore_missing, unicode],
-        'password': [user_password_validator, user_password_not_empty, ignore_missing, unicode],
-        'password_hash': [ignore_missing, ignore_not_sysadmin, unicode],
-        'email': [not_empty, unicode],
-        'about': [ignore_missing, user_about_validator, unicode],
+        'id': [ignore_missing, unicode_safe],
+        'name': [not_empty, adfs_name_validator, user_name_validator,
+                 adfs_user_name_sanitize, unicode_safe],
+        'fullname': [ignore_missing, unicode_safe],
+        'password': [adfs_user_password_validator, user_password_not_empty,
+                     ignore_missing, unicode_safe],
+        'password_hash': [ignore_missing, ignore_not_sysadmin, unicode_safe],
+        'email': [not_empty, unicode_safe, email_validator],
+        'about': [ignore_missing, user_about_validator, unicode_safe],
         'created': [ignore],
         'openid': [ignore_missing],
         'sysadmin': [ignore_missing, ignore_not_sysadmin],
@@ -35,6 +41,9 @@ def default_user_schema():
         'reset_key': [ignore],
         'activity_streams_email_notifications': [ignore_missing],
         'state': [ignore_missing],
+        'image_url': [ignore_missing, unicode_safe],
+        'image_display_url': [ignore_missing, unicode_safe],
+        'plugin_extras': [ignore_missing, json_object, ignore_not_sysadmin]
     }
     return schema
 
@@ -42,11 +51,13 @@ def default_user_schema():
 def user_new_form_schema():
     schema = default_user_schema()
 
-    schema['name'] = [not_empty, adfs_name_validator, user_name_validator, adfs_user_name_sanitize, unicode]
-    schema['fullname'] = [ignore_missing, adfs_user_name_sanitize, unicode]
-    schema['about'] = [ignore_missing, user_about_validator, adfs_user_about_validator, unicode]
-    schema['password1'] = [unicode, user_both_passwords_entered, user_password_validator, user_passwords_match]
-    schema['password2'] = [unicode]
+    schema['name'] = [not_empty, adfs_name_validator, user_name_validator,
+                      adfs_user_name_sanitize, unicode_safe]
+    schema['fullname'] = [ignore_missing, adfs_user_name_sanitize, unicode_safe]
+    schema['about'] = [ignore_missing, user_about_validator, unicode_safe]
+    schema['password1'] = [unicode_safe, user_both_passwords_entered,
+                           adfs_user_password_validator, user_passwords_match]
+    schema['password2'] = [unicode_safe]
 
     return schema
 
@@ -54,12 +65,14 @@ def user_new_form_schema():
 def user_edit_form_schema():
     schema = default_user_schema()
 
-    schema['name'] = [ignore_missing, adfs_name_validator, user_name_validator, adfs_user_name_sanitize, unicode]
-    schema['fullname'] = [ignore_missing, adfs_user_name_sanitize, unicode]
-    schema['about'] = [ignore_missing, user_about_validator, adfs_user_about_validator, unicode]
+    schema['name'] = [ignore_missing, adfs_name_validator, user_name_validator,
+                      adfs_user_name_sanitize, adfs_old_username_validator, unicode_safe]
+    schema['fullname'] = [ignore_missing, adfs_user_name_sanitize, unicode_safe]
+    schema['about'] = [ignore_missing, user_about_validator, unicode_safe]
     schema['password'] = [ignore_missing]
-    schema['password1'] = [ignore_missing, unicode, user_password_validator, user_passwords_match]
-    schema['password2'] = [ignore_missing, unicode]
+    schema['password1'] = [ignore_missing, unicode_safe, adfs_user_password_validator,
+                           user_passwords_match, adfs_password_validator]
+    schema['password2'] = [ignore_missing, unicode_safe]
 
     return schema
 
@@ -67,7 +80,8 @@ def user_edit_form_schema():
 def default_update_user_schema():
     schema = default_user_schema()
 
-    schema['name'] = [ignore_missing, adfs_name_validator, user_name_validator, unicode]
-    schema['password'] = [user_password_validator, ignore_missing, unicode]
-
+    schema['name'] = [ignore_missing, adfs_name_validator,
+                      user_name_validator, unicode_safe]
+    schema['password'] = [ignore_missing, adfs_user_password_validator,
+                          adfs_password_validator, unicode_safe]
     return schema
--- ckanext/adfs/validators.py
+++ ckanext/adfs/validators.py
@@ -1,17 +1,92 @@
+import json
 import re
+import six
+import string
+from ckan import authz
 from ckan.common import _
+from ckan.lib.navl.dictization_functions import Missing, Invalid
 from ckan.model import PACKAGE_NAME_MAX_LENGTH
-from ckan.lib.navl.dictization_functions import Invalid
 from profanityfilter import ProfanityFilter
+
+
+MIN_PASSWORD_LENGTH = 10
+MIN_RULE_SETS = 2
+MIN_LEN_ERROR = (
+    'Your password must be {} characters or longer, and consist of at least '
+    '{} of the following character sets: uppercase characters, lowercase '
+    'characters, digits, punctuation & special characters.'
+)
+
+
+def adfs_user_password_validator(key, data, errors, context):
+    value = data[key]
+
+    if isinstance(value, Missing):
+        pass
+    elif not isinstance(value, six.string_types):
+        raise Invalid(_('Passwords must be strings.'))
+    elif value == '':
+        pass
+    else:
+        rules = [
+            any(x.isupper() for x in value),
+            any(x.islower() for x in value),
+            any(x.isdigit() for x in value),
+            any(x in string.punctuation for x in value)
+        ]
+        if len(value) < MIN_PASSWORD_LENGTH or sum(rules) < MIN_RULE_SETS:
+            raise Invalid(_(MIN_LEN_ERROR.format(MIN_PASSWORD_LENGTH, MIN_RULE_SETS)))
+
+
+def adfs_old_username_validator(key, data, errors, context):
+    # Prevents changing of user names
+    user_id = data.get(('id',))
+    old_user = context['model'].User.get(user_id)
+    new_user_name = data[key]
+    if old_user.name != new_user_name:
+        if is_adfs_user(old_user.name, context):
+            raise Invalid(_('Unauthorized to change user name'))
+        if not authz.is_sysadmin(context.get('user')):
+            raise Invalid(_('Unauthorized to change user name'))
+    return old_user.name
+
+
+def adfs_user_name_sanitize(key, data, errors, context):
+    value = data[key]
+    if is_input_valid(value) is False:
+        raise Invalid(_('Input contains invalid text'))
+    elif value and re.match('admin', value, re.IGNORECASE):
+        raise Invalid(_('Input contains invalid text'))
+    elif value and re.match('edit', value, re.IGNORECASE):
+        raise Invalid(_('Input contains invalid text'))
+    elif value and re.match('me', value, re.IGNORECASE):
+        raise Invalid(_('Input contains invalid text'))
+
+
+invalid_list = [
+    'activity', 'delete', 'follow', 'followers', 'generate_key', 'hack',
+    'login', 'logged_in', 'logged_out', 'logged_out_redirect',
+    'malware', 'register', 'reset', 'root', 'set_lang', 'unfollow', 'virus',
+    '_logout',
+]
+def is_input_valid(input_value):
+    value = input_value.lower()
+    pf = ProfanityFilter()
+    for invalid_string in invalid_list:
+        if re.search(invalid_string, value, re.IGNORECASE):
+            return False
+    if not pf.is_clean(value):
+        return False
+    return True
 
 
 name_match = re.compile('[a-z0-9_\-]*$')
 def adfs_name_validator(value, context):
-    """
+    '''
     Custom validator for usernames
     ADFS users have email addresses as usernames
-    """
-    if not isinstance(value, basestring):
+    '''
+    if not isinstance(value, six.string_types):
         raise Invalid(_('Names must be strings'))
 
     # check basic textual rules
@@ -34,46 +109,50 @@ def adfs_name_validator(value, context):
     return value
 
 
-def adfs_user_name_sanitize(key, data, errors, context):
-    # Non-ADFS users need to sanitize their username
-    username = context.get('user')
-    if not is_adfs_user(username, context):
-        invalid_name = ['admin', 'manage', 'root']
-        value = data[key]
-        if is_input_valid(value) is False:
-            raise Invalid(_('Input Contains Invalid Text'))
-        for invalid_string in invalid_name:
-            if value and re.match(invalid_string, value, re.IGNORECASE):
-                raise Invalid(_('Input Contains Invalid Text'))
+def adfs_password_validator(key, data, errors, context):
+    '''
+    ADFS users do not have passwords and can not reset passwords
+    '''
+    model = context['model']
+    user = context.get('user')
 
+    password = data.get(('password',),None)
 
-def adfs_user_about_validator(key, data, errors, context):
-    value = data[key]
-    if is_input_valid(value) is False:
-        raise Invalid(_('Input Contains Invalid Text'))
+    password1 = data.get(('password1',),None)
+    password2 = data.get(('password2',),None)
 
+    if password and is_adfs_user(user, context):
+        raise Invalid(_('Unauthorized to set password'))
 
-def is_input_valid(input_value):
-    invalid_list = ['hacked', 'hacking', 'hacks', 'hack[^a-zA-Z]+', 'malware', 'virus']
-    pf = ProfanityFilter()
-
-    for invalid_string in invalid_list:
-        if re.search(invalid_string, input_value, re.IGNORECASE):
-            return False
-    if not pf.is_clean(input_value):
-        return False
-    return True
+    if password1 and password1 == password2 and is_adfs_user(user, context):
+        raise Invalid(_('Unauthorized to set password'))
 
 
 def is_adfs_user(username, context):
+    '''
+    ADFS users have email addresses as usernames
+    '''
     if not username:
         username = context.get('user')
-    # ADFS users have email addresses as usernames
     if re.match("[^@]+@[^@]+\.[^@]+", username, re.IGNORECASE):
         model = context['model']
         user = model.User.by_name(username)
-        # ADFS users do not have passwords
         if user:
             if user.is_active() and not user.password:
                 return True
     return False
+
+
+def json_object(value):
+    '''
+    Make sure value can be serialized as a JSON object
+    '''
+    if value is None or value == '':
+        return
+    try:
+        if not json.dumps(value).startswith('{'):
+            raise Invalid(_('The value should be a valid JSON object'))
+    except ValueError as e:
+        raise Invalid(_('Could not parse the value as a valid JSON object'))
+
+    return value
